fa26a610634728b5d997ca47ca0112635222a7cf
Kristin Skipper <kskipper.03@gmail.com>
2020-07-07 12:41:37 -0500

    added python bot message

--- src/bot.py
+++ src/bot.py
@@ -6,24 +6,28 @@ from dotenv import load_dotenv
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
-my_guild = os.getenv('DISCORD_GUILD')
+#my_guild = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == my_guild:
-            break
+    print(f'{client.user.name} has connected to Discord!')
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
+@client.event
+async  def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
     )
+    # print(
+    #     f'{client.user} is connected to the following guild:\n'
+    #     f'{guild.name}(id: {guild.id})'
+    # )
 
-    member = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {member}')
+    # member = '\n - '.join([member.name for member in guild.members])
+    # print(f'Guild Members:\n - {member}')
 
 
 client.run(token)
