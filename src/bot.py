e3b0c0b67956c771174e47cfbf8e12375c4054cf
Kristin Skipper <kskipper.03@gmail.com>
2020-07-07 13:09:34 -0500

    added random quotes for bot to chose from'

--- src/bot.py
+++ src/bot.py
@@ -1,5 +1,6 @@
 # bot.py
 import os
+import random
 
 import discord
 from dotenv import load_dotenv
@@ -21,6 +22,23 @@ async  def on_member_join(member):
     await member.dm_channel.send(
         f'Hi {member.name}, welcome to my Discord server!'
     )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 100 emoji.', 'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
     # print(
     #     f'{client.user} is connected to the following guild:\n'
     #     f'{guild.name}(id: {guild.id})'
